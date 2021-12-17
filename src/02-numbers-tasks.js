ea473936172a9937b02ee18fe38b0cd7efc01761
DarMozh <dariamozhilovskaya@gmail.com>
2021-12-17 23:44:47 +0300

    24

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -195,8 +195,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 3) return n > 1;
+  if ((n % 2 === 0) || (n % 3 === 0)) return false;
+  let count = 5;
+  while (count ** 2 <= n) {
+    if (n % count === 0 || n % (count + 2) === 0) return false;
+    count += 6;
+  }
+  return true;
 }
 
 /**
