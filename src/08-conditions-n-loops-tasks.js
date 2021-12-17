d176fae0aebc00dea7e316095356887b4b0ea696
DarMozh <dariamozhilovskaya@gmail.com>
2021-12-18 00:35:45 +0300

    85

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -302,8 +302,13 @@ function getDigitalRoot(num) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
+function isBracketsBalanced(str) {
+  if (str.length === 0) return true;
+  const ind = Math.max(str.lastIndexOf('['), str.lastIndexOf('{'), str.lastIndexOf('('), str.lastIndexOf('<'));
+  const nextChar = (str.charAt(ind) === '(') ? ')' : String.fromCharCode(str.charCodeAt(ind) + 2);
+  if (str.charAt(ind + 1) !== nextChar) return false;
+  str = str.substring(0, ind) + str.substring(ind + 2);
+  return isBracketsBalanced(str);
 }
 
 /**
